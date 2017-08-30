cadc7d0f94101c0d0d775bcafbed90293c75467a
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-08-30 14:55:29 +0200

    Change error handling

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const ipfsd = require('ipfsd-ctl')
 console.log('running');
 
 ipfsd.disposableApi((err, ipfs) => {
-    if (err !== null) {
-        console.log('err:', err)
-        process.exit();
-    }
+    if (err) { throw err }
 
     ipfs.id(function (err, id) {
+        if (err) { throw err }
         console.log(id);
     });
 
     ipfs.pin.ls({'type': 'recursive'}).then(items => {
         for (var hash in items) {
             ipfs.pin.rm(hash, {'recursive': true}).then((err, unpinned) => {
-                console.log('err:', err)
+                if (err) { throw err }
                 console.log('unpinned:', unpinned)
             })
             console.log('hash', hash);
