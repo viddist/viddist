c435887de884407597b697d222cf5eb27459ede3
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-09-08 16:04:59 +0200

    Publish debug code

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const ipfsd = require('ipfsd-ctl')
 
 console.log('running');
 
-ipfsd.disposableApi((err, ipfs) => {
+ipfsd.local((err, ipfs) => {
     if (err) { throw err }
 
-    ipfs.id(function (err, id) {
-        if (err) { throw err }
-        console.log(id);
-    });
+    //console.log(ipfs)
+    console.log(ipfs.apiAddr)
+    console.log(ipfs.gatewayAddr)
 
-    ipfs.pin.ls({'type': 'recursive'}).then(items => {
-        for (var hash in items) {
-            ipfs.pin.rm(hash, {'recursive': true}).then((err, unpinned) => {
-                if (err) { throw err }
-                console.log('unpinned:', unpinned)
-            })
-            console.log('hash', hash);
-        }
-    });
+    //ipfs.id(function (err, id) {
+    //    if (err) { throw err }
+    //    console.log(id);
+    //});
+
+    //ipfs.pin.ls({'type': 'recursive'}).then(items => {
+    //    for (var hash in items) {
+    //        ipfs.pin.rm(hash, {'recursive': true}).then((err, unpinned) => {
+    //            if (err) { throw err }
+    //            console.log('unpinned:', unpinned)
+    //        })
+    //        console.log('hash', hash);
+    //    }
+    //});
 });
