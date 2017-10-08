cf13e211682b3a72d0e20fc1409ac6eea2f00b63
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-10-08 22:28:22 +0200

    Revert "Add the result of an ipfs cat to html"
    This reverts commit 40ef1705cbf70c5eb4d3eb449f5aa0254965932d.

--- index.js
+++ index.js
@@ -2,36 +2,21 @@ const ipfsd = require('ipfsd-ctl')
 
 console.log('running');
 
-const imgHash = 'QmZZbDoceKrFmjAYKeL4WfkBvexvzpff6yZTga2W8qoUF2'
-const textHash = 'QmRmPLc1FsPAn8F8F9DQDEYADNX5ER2sgqiokEvqnYknVW'
-
 ipfsd.disposableApi((err, ipfs) => {
     if (err) { throw err }
 
     ipfs.id(function (err, id) {
         if (err) { throw err }
-        console.log(id)
-    })
+        console.log(id);
+    });
 
-    ipfs.files.cat(textHash, (err, stream) => {
-        if (err) { throw err }
-        console.log(stream)
-
-        stream.setEncoding('utf8')
-        stream.on('data', function(chunk) {
-            console.log(chunk)
-            document.getElementById('content').innerHTML = chunk
-        })
-        //stream.pipe(console.log)
-    })
-
-    //ipfs.pin.ls({'type': 'recursive'}).then(items => {
-    //    for (var hash in items) {
-    //        ipfs.pin.rm(hash, {'recursive': true}).then((err, unpinned) => {
-    //            if (err) { throw err }
-    //            console.log('unpinned:', unpinned)
-    //        })
-    //        console.log('hash', hash);
-    //    }
-    //});
+    ipfs.pin.ls({'type': 'recursive'}).then(items => {
+        for (var hash in items) {
+            ipfs.pin.rm(hash, {'recursive': true}).then((err, unpinned) => {
+                if (err) { throw err }
+                console.log('unpinned:', unpinned)
+            })
+            console.log('hash', hash);
+        }
+    });
 });
