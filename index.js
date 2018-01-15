f917d5d3fff38a76bb6216981603c0fc0f257a40
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-01-15 17:28:01 +0100

    Manage to render images using this method

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ ipfsd.disposableApi((err, ipfs) => {
 
     insertIpfsFile(ipfs, 'textContent', 'text', textHash)
 
-    //insertIpfsFile(ipfs, 'imgContent', 'png', imgHash)
+    insertIpfsFile(ipfs, 'imgContent', 'png', imgHash)
 
     insertIpfsFile(ipfs, 'vidContent', 'mp4', vidHash)
 })
@@ -31,16 +31,33 @@ ipfsd.disposableApi((err, ipfs) => {
 const insertIpfsFile = (ipfs, domId, type, hash) => {
     const stream = ipfs.files.catReadableStream(hash)
     if (type === 'text') {
-        const text = stream.toString()
-        console.log('received text: ', text)
-        document.getElementById(domId).innerHTML = text
-    //} else if (type === 'png') {
+        //const text = stream.toString()
+        //console.log('received text: ', text)
+        console.log("Trying to pipe to stdout:")
+        //process.stdout.write(stream)
+        stream.pipe(process.stdout)
+        //document.getElementById(domId).innerHTML = text
+    } else if (type === 'png') {
     //    const blob = new Blob([data],
     //        { type: 'image/png' } )
     //    const imageUrl = window.URL.createObjectURL(blob)
     //    const imgElem = document.createElement('img')
     //    imgElem.src = imageUrl
     //    document.getElementById(domId).appendChild(imgElem)
+        renderMedia.append({
+            name: 'image.png',
+            createReadStream: (opts) => {
+                if(opts) {
+                    for (const o in opts) {
+                        console.log("drawO: " + o)
+                    }
+                }
+                console.log("drawing")
+                return stream
+            }},
+            document.getElementById(domId),
+            {maxBlobLength: 1000},
+            (err, elem) => console.log("nice2"))
     } else if (type === 'mp4') {
         //const blob = new Blob([data],
         //    { type: 'video/mp4' } )
@@ -52,12 +69,16 @@ const insertIpfsFile = (ipfs, domId, type, hash) => {
         renderMedia.append({
             name: 'video.mp4',
             createReadStream: (opts) => {
-                if(opts) console.log("Opts: " + opts)
+                if(opts) {
+                    for (const o in opts) {
+                        console.log("renderO: " + o + ": " + opts[o])
+                    }
+                }
                 console.log("rendering")
                 return stream
             }},
             document.getElementById(domId),
-            //{maxBlobLength: 1000 * 1000},
+            {maxBlobLength: 1000 * 1000},
             (err, elem) => console.log("nice"))
     } else {
         console.error("This isn't supposed to happen")
