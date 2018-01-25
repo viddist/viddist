327886eaff33d9c98e5a0b7e88dd0f72f2bd1df0
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-01-25 15:29:35 +0100

    Add big buck bunny and transcode videos to webopt

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const imgHash = 'QmZZbDoceKrFmjAYKeL4WfkBvexvzpff6yZTga2W8qoUF2'
 // The logo inside a dir, named code.png
 const imgDirHash = 'QmYGVR5U4EmqZxoLQnMqRdG2nZ25PrATBCcJJaXLiQH6Ta'
 // Sintel with subtitles 130MB
-const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
-// Big buck bunny 480p avi transcoded to mp4 using handbrake
-//const vidHash =
+//const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
+// Sintel transcoded to webopt using handbrake
+const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
+// Big buck bunny 480p avi transcoded to mp4 webopt using handbrake
+//const vidHash = 'QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4'
 
 daemonFactory.spawn({disposable: true}, (err, ipfsd) => {
     if (err) { console.error(err) }
