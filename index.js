8f3e6046ca8205637b4ba32daa9144bf0b4b5955
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-01-30 20:17:57 +0100

    Remove unused image and text code

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const daemonFactory = require('ipfsd-ctl').create({type: 'go'})
 
 console.log('running')
 
-// A text file containing "hello test"
-const textHash = 'QmRmPLc1FsPAn8F8F9DQDEYADNX5ER2sgqiokEvqnYknVW'
-// The new code@lth logo on fb
-const imgHash = 'QmZZbDoceKrFmjAYKeL4WfkBvexvzpff6yZTga2W8qoUF2'
-// The logo inside a dir, named code.png
-//const imgDirHash = 'QmYGVR5U4EmqZxoLQnMqRdG2nZ25PrATBCcJJaXLiQH6Ta'
 // Sintel with subtitles 130MB
 //const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
 // Sintel transcoded to webopt using handbrake
@@ -29,48 +23,29 @@ daemonFactory.spawn({disposable: true}, (err, ipfsd) => {
 
     initUserProfile(ipfs)
 
-    insertIpfsFile(ipfs, 'textContent', 'text', textHash)
-
-    insertIpfsFile(ipfs, 'imgContent', 'png', imgHash)
-
-    insertIpfsFile(ipfs, 'vidContent', 'mp4', vidHash)
+    playVideo(ipfs, vidHash)
 
     document.getElementById('video-address-form')
         .addEventListener('submit', e => {
             e.preventDefault()
             const newVid = document.getElementById('video-address').value
             console.log('Address: ' + newVid)
-            insertIpfsFile(ipfs, 'vidContent', 'mp4', newVid)
+            playVideo(ipfs, newVid)
             document.getElementById('video-address').value = ''
         })
 })
 
-const insertIpfsFile = (ipfs, domId, type, hash) => {
+const playVideo = (ipfs, hash) => {
     ipfs.files.cat(hash).then(data => {
-        if (type === 'text') {
-            const text = data.toString()
-            console.log('received text: ', text)
-            document.getElementById(domId).innerHTML = text
-        } else if (type === 'png') {
-            const blob = new Blob([data],
-                { type: 'image/png' } )
-            const imageUrl = window.URL.createObjectURL(blob)
-            const imgElem = document.createElement('img')
-            imgElem.src = imageUrl
-            document.getElementById(domId).appendChild(imgElem)
-        } else if (type === 'mp4') {
-            document.getElementById('playing-video').outerHTML = ''
-            const blob = new Blob([data],
-                { type: 'video/mp4' } )
-            const vidElem = document.createElement('video')
-            vidElem.controls = true
-            vidElem.autoplay = true
-            vidElem.id = 'playing-video'
-            vidElem.src = window.URL.createObjectURL(blob)
-            document.getElementById(domId).appendChild(vidElem)
-        } else {
-            console.error('Unsupported content type')
-        }
+        document.getElementById('playing-video').outerHTML = ''
+        const blob = new Blob([data],
+            { type: 'video/mp4' } )
+        const vidElem = document.createElement('video')
+        vidElem.controls = true
+        vidElem.autoplay = true
+        vidElem.id = 'playing-video'
+        vidElem.src = window.URL.createObjectURL(blob)
+        document.getElementById('vidContent').appendChild(vidElem)
     }).catch(console.error)
 }
 
