868c082f78cf76e13b3124ad9ad5ef2b2838f83e
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-01-31 01:44:29 +0100

    Add blinkenlights

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const daemonFactory = require('ipfsd-ctl').create({type: 'go'})
 
 console.log('running')
 
+// From the CCC archives, pinned by others but recommended to pin locally to
+// speed up testing. (I don't recommend pinning the top hash, it's several TBs)
+// Blinkenlights 1
+const vidHash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/blinkenlights-video-large.mp4'
+// Blinkenlights 2
+//const vidhash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/P-Link.mp4'
+// NOTE: The hashes below here are things that are probably offline. Contact
+// me and I'll bring up my daemon for a bit so you can pin them.
 // Sintel with subtitles 130MB
 //const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
 // Sintel transcoded to webopt using handbrake
-const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
+//const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
 // Big buck bunny 480p avi transcoded to mp4 webopt using handbrake
 //const vidHash = 'QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4'
 
@@ -37,7 +45,7 @@ daemonFactory.spawn({disposable: true}, (err, ipfsd) => {
 
     byId('video-address-form').addEventListener('submit', e => {
         e.preventDefault()
-        const newVid = byId('video-address').value
+        const newVid = byId('new-video-address').value
         console.log('Address: ' + newVid)
         playVideo(ipfs, newVid)
         byId('new-video-address').value = ''
