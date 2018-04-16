175738e8857c0e692624b7a9ab04697b31cea7d8
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-04-17 00:01:50 +0200

    Fix Standard complaints

--- index.js
+++ index.js
@@ -8,109 +8,109 @@ console.log('running')
 // Blinkenlights 1
 const vidHash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/blinkenlights-video-large.mp4'
 // Blinkenlights 2
-//const vidhash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/P-Link.mp4'
+// const vidhash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/P-Link.mp4'
 // NOTE: The hashes below here are things that are probably offline. Contact
 // me and I'll bring up my daemon for a bit so you can pin them.
 // Sintel with subtitles 130MB
-//const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
+// const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
 // Sintel transcoded to webopt using handbrake
-//const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
+// const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
 // Big buck bunny 480p avi transcoded to mp4 webopt using handbrake
-//const vidHash = 'QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4'
+// const vidHash = 'QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4'
 
 const protocolVersion = '1'
 const userAddressKeyName = 'user-address'
 let ipfs
 
 daemonFactory.spawn({disposable: true}, (err, ipfsd) => {
-    if (err) { console.error(err) }
-    ipfs = ipfsd.api
+  if (err) { console.error(err) }
+  ipfs = ipfsd.api
 
-    ipfs.id().then(console.log).catch(console.error)
+  ipfs.id().then(console.log).catch(console.error)
 
-    initUserProfile()
+  initUserProfile()
 
-    playVideo(vidHash)
+  playVideo(vidHash)
 
-    byId('other-user-address-form').addEventListener('submit', e => {
-        e.preventDefault()
-        const otherUserId = byId('other-user-address').value
-        const userProfileFile = otherUserId + '/user-profile.json'
-        ipfs.name.resolve(userProfileFile).then(address => {
-            return ipfs.files.cat(address)
-        }).then(data => {
-            byId('other-user-profile').innerText = data.toString()
-        }).catch(console.error)
-    })
+  byId('other-user-address-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const otherUserId = byId('other-user-address').value
+    const userProfileFile = otherUserId + '/user-profile.json'
+    ipfs.name.resolve(userProfileFile).then(address => {
+      return ipfs.files.cat(address)
+    }).then(data => {
+      byId('other-user-profile').innerText = data.toString()
+    }).catch(console.error)
+  })
 
-    byId('video-address-form').addEventListener('submit', e => {
-        e.preventDefault()
-        const newVid = byId('new-video-address').value
-        console.log('Address: ' + newVid)
-        playVideo(newVid)
-        byId('new-video-address').value = ''
-    })
+  byId('video-address-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const newVid = byId('new-video-address').value
+    console.log('Address: ' + newVid)
+    playVideo(newVid)
+    byId('new-video-address').value = ''
+  })
 })
 
 const byId = id => document.getElementById(id)
 
 const playVideo = hash => {
-    ipfs.files.cat(hash).then(data => {
-        byId('playing-video').outerHTML = ''
-        const blob = new Blob([data],
-            { type: 'video/mp4' } )
-        const vidElem = document.createElement('video')
-        vidElem.controls = true
-        vidElem.autoplay = true
-        vidElem.muted = true
-        vidElem.id = 'playing-video'
-        vidElem.src = window.URL.createObjectURL(blob)
-        byId('vidContent').appendChild(vidElem)
-    }).catch(console.error)
+  ipfs.files.cat(hash).then(data => {
+    byId('playing-video').outerHTML = ''
+    const blob = new window.Blob([data],
+      { type: 'video/mp4' })
+    const vidElem = document.createElement('video')
+    vidElem.controls = true
+    vidElem.autoplay = true
+    vidElem.muted = true
+    vidElem.id = 'playing-video'
+    vidElem.src = window.URL.createObjectURL(blob)
+    byId('vidContent').appendChild(vidElem)
+  }).catch(console.error)
 }
 
 const initUserProfile = () => {
-    ipfs.key.list().then(keys => {
-        let alreadyInited = false
-        keys.forEach(key => {
-            if (key.name === userAddressKeyName) {
-                byId('user-address').innerText = key.id
-                alreadyInited = true
-            }
-        })
-        if (alreadyInited) {
-            return Promise.resolve()
-        }
-        // Initialize the user profile key and files if they don't exist
-        ipfs.key.gen(userAddressKeyName, {
-            type: 'rsa',
-            size: 2048
-        }).then(key => {
-            byId('user-address').innerText = key.id
-            return addUserProfile({
-                userName: 'Viddist ~ö~ User',
-                pinnedVids: [] })
-        }).then(res => {
-            const dirHash = res[2].hash // This has worked so far but watch out
-            return Promise.all([
-                ipfs.pin.add(dirHash, {recursive: true}).hash,
-                ipfs.name.publish(dirHash, {key: userAddressKeyName})
-            ])
-        }).then(res => {
-            console.log('Published to profile')
-            console.log(res[1])
-        }).catch(console.error)
+  ipfs.key.list().then(keys => {
+    let alreadyInited = false
+    keys.forEach(key => {
+      if (key.name === userAddressKeyName) {
+        byId('user-address').innerText = key.id
+        alreadyInited = true
+      }
+    })
+    if (alreadyInited) {
+      return Promise.resolve()
+    }
+    // Initialize the user profile key and files if they don't exist
+    ipfs.key.gen(userAddressKeyName, {
+      type: 'rsa',
+      size: 2048
+    }).then(key => {
+      byId('user-address').innerText = key.id
+      return addUserProfile({
+        userName: 'Viddist ~ö~ User',
+        pinnedVids: [] })
+    }).then(res => {
+      const dirHash = res[2].hash // This has worked so far but watch out
+      return Promise.all([
+        ipfs.pin.add(dirHash, {recursive: true}).hash,
+        ipfs.name.publish(dirHash, {key: userAddressKeyName})
+      ])
+    }).then(res => {
+      console.log('Published to profile')
+      console.log(res[1])
     }).catch(console.error)
+  }).catch(console.error)
 }
 
 const addUserProfile = profile => {
-    return ipfs.files.add([{
-        path: '/viddist-meta/viddist-version.txt',
-        content: Buffer.from(protocolVersion, 'utf-8')
-    } , { // I love that ESLint doesn't complain about anything here
-        path:'/viddist-meta/user-profile.json',
-        content: Buffer.from(JSON.stringify(profile))
-    }])
+  return ipfs.files.add([{
+    path: '/viddist-meta/viddist-version.txt',
+    content: Buffer.from(protocolVersion, 'utf-8')
+  }, { // I love that ESLint/Standard don't complain about anything here
+    path: '/viddist-meta/user-profile.json',
+    content: Buffer.from(JSON.stringify(profile))
+  }])
 }
 
-//const updateUserProfile = 
+// const updateUserProfile =
