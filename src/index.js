e6095a2cbc28c71b0e3bc074763850c0784ae73c
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-07-29 18:49:09 +0200

    Add test-videos.md

--- src/index.js
+++ src/index.js
@@ -5,20 +5,9 @@ const choo = require('choo')
 const main = require('./templates/main.js')
 const profile = require('./my-profile.js')
 
-// From the CCC archives, pinned by others but recommended to pin locally to
-// speed up testing. (I don't recommend pinning the top hash, it's several TBs)
-// Blinkenlights 1
-// const vidHash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/blinkenlights-video-large.mp4'
-// Blinkenlights 2
+// "Blinkenlights 2"
 const vidHash = 'QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/P-Link.mp4'
-// NOTE: The hashes below here are things that are probably offline. Contact
-// me and I'll bring up my daemon for a bit so you can pin them.
-// Sintel with subtitles 130MB
-// const vidHash = 'QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4'
-// Sintel transcoded to webopt using handbrake
-// const vidHash = 'QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4'
-// Big buck bunny 480p avi transcoded to mp4 webopt using handbrake
-// const vidHash = 'QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4'
+// See test-videos.md for more videos to use
 
 let ipfs
 const train = choo()
---
+++ test-videos.md
@@ -0,0 +1,20 @@
+# Testing videos
+
+These are videos hosted on ipfs that you can use for testing
+
+These are from the CCC archives, pinned by others but recommended to pin locally to
+speed up testing. (I don't recommend pinning the top hash, it is several TBs
+large)
+* "Blinkenlights 1"
+ `QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/blinkenlights-video-large.mp4`
+* Blinkenlights 2
+`QmW84mqTYnCkRTy6VeRJebPWuuk8b27PJ4bWm2bL4nrEWb/blinkenlights/mp4/P-Link.mp4`
+
+The videos below are things that are probably offline. Contact
+me and I'll bring up my daemon for a bit so you can pin them.
+* Sintel, with subtitles, 130MB
+`QmXgfYJiG3JttNqVHERfGysc1semSrFnhtAiFU1C6oLNWW/Sintel.mp4`
+* Sintel, transcoded to webopt using handbrake
+`QmcPmxZUFNLpwgYcGExARE8ZYgNEr1oSTMTSut1ZhfK6us/sintel-webopt.mp4`
+* Big buck bunny, 480p .avi transcoded to mp4 webopt using handbrake
+`QmNnpS4RXHBMKhTY1s1gHn41xmXiWJEAakmM5uMZ66HfGx/big-buck-bunny-480p-webopt.mp4`
