770fa7307e8469852bdeebbcbd0770b9e51ac581
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-08-19 14:51:26 +0200

    Play video

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 const choo = require('choo')
 
 const main = require('./templates/main.js')
-const profile = require('./my-profile.js')
+// const profile = require('./my-profile.js')
 
-const css = require("sheetify")
+const css = require('sheetify')
 css('./index.css')
 
 const train = choo()
@@ -15,13 +15,13 @@ train.use(async (state, emitter) => {
   state.videoLink = ''
   state.videoAddress = ''
 
-  state.myProfileAddress = await profile.init(ipfs)
+  // state.myProfileAddress = await profile.init()
   emitter.emit('render')
 
-  //emitter.on('viewProfile', async userId => {
-  //  state.otherUserProfile = await profile.getUser(userId)
-  //  emitter.emit('render')
-  //})
+  // emitter.on('viewProfile', async userId => {
+  //   state.otherUserProfile = await profile.getUser(userId)
+  //   emitter.emit('render')
+  // })
 
   emitter.on('playNewVideo', async newVid => {
     state.videoAddress = newVid
@@ -29,12 +29,12 @@ train.use(async (state, emitter) => {
     emitter.emit('render')
   })
 
-  //emitter.on('pinVideo', async videoLink => {
+  // emitter.on('pinVideo', async videoLink => {
   //  await profile.pinVideo(videoLink)
   //  console.log('Pinned current video')
-  //})
+  // })
 
-  emitter.emit('playNewVideo', vidHash)
+  emitter.emit('playNewVideo', 'dat://16d7782cda79cf1c35245b317f101a2ad1ea9c9fba3a29a70f4502629b8efa4e/Sintel.mp4')
 })
 
 train.route('/', main)
