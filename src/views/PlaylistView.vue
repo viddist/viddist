99dc72182fd7fc4caa263de73b1faba252d04f09
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-12-09 23:54:30 +0100

    Add playlist view, and remove old code

--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -1,18 +1,49 @@
 <template>
   <div>
     <router-link :to="{ name: 'home' }">Home</router-link>
-    <!--<h3>${playlist.username}</h3>
+    <h3>{{ name }}</h3>
+    Playlist url: <code>{{ playlistUrl }}</code>
     <ul>
-    ${playlist.videoList.map(videoItem)}
-    </ul>-->
-    playlist {{ playlistUrl }}
+      <li v-for="url in videoList" :key="url">
+        <button @click="playVideo(url)">
+          {{ url }}
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+const dat = window.DatArchive
+
 export default {
   props: {
     playlistUrl: String,
+  },
+  data () {
+    return {
+      name: null,
+      videoList: null,
+    }
+  },
+  methods: {
+    playVideo (url) {
+      this.$router.push({
+        name: 'video',
+        params: { videoUrl: url }
+      })
+    },
+  },
+  watch: {
+    playlistUrl: {
+      immediate: true,
+      async handler (url) {
+        const archive = await dat.load(url)
+
+        this.name = await archive.readFile('/name.txt')
+        this.videoList = JSON.parse(await archive.readFile('/videoList.json'))
+      }
+    },
   },
 }
 </script>
